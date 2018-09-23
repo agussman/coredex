0e19aad8f256a0723fa05715c82913c1acc5b121
Aaron Gussman <agussman@gmail.com>
2018-09-23 16:41:39 -0400

    Set id on edge creation

--- csv2neptune.py
+++ csv2neptune.py
@@ -58,9 +58,8 @@ def main():
             to_id = id_transform(row["~to"])
             print(edge_id)
 
-            e = g.V(from_id).addE(row["~label"]).to( g.V(to_id) ).next()
+            e = g.V(from_id).addE(row["~label"]).to( g.V(to_id) ).property(T.id, edge_id).next()
 
-            # e = g.addE(row["~label"]).from(from_id).to(to_id).property(T.id, edge_id)
             for key in row:
                 if key.startswith('~'):
                     continue
