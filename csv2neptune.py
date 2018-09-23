e3dcd6d82c70f914304f3705abc9e6bb26b4672b
Aaron Gussman <agussman@gmail.com>
2018-09-23 16:06:03 -0400

    Code to add edges from csv

--- csv2neptune.py
+++ csv2neptune.py
@@ -25,11 +25,15 @@ logging.basicConfig()
 LOG = logging.getLogger('csv2neptune')
 LOG.setLevel(logging.DEBUG)
 
+def id_transform(original_id):
+    return "id_"+original_id
+
 def main():
     # Read the variables
     args = parse_options()
     neptune = args.neptune
     v_file = args.vertices
+    e_file = args.edges
 
     # Connect to Neptune
     neptune_constr = "ws://%s/gremlin" % neptune
@@ -43,8 +47,8 @@ def main():
     with open(v_file) as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
         for row in reader:
-            myid = "id_"+row["~id"]
-            print(row["~id"])
+            myid = id_transform(row["~id"])
+            print(myid)
             v = g.addV(row["~label"]).property(T.id, myid)
             #v.property("foo", "bar")
             #v.property("ack", "syn")
@@ -54,6 +58,22 @@ def main():
                 plabel, ptype = key.split(':')
                 v.property(plabel, row[key])
             v.next()
+
+    # Load the edges
+    with open(e_file) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+        for row in reader:
+            edge_id = id_transform(row["~id"])
+            from_id = id_transform(row["~from"])
+            to_id = id_transform(row["~to"])
+            print(myid)
+            e = g.addE(row["~label"]).from(from_id).to(to_id).property(T.id, edge_id)
+            for key in row:
+                if key.startswith('~'):
+                    continue
+                plabel, ptype = key.split(':')
+                e.property(plabel, row[key])
+            e.next()
 
 
     print(g.V().limit(2).toList())
@@ -66,6 +86,7 @@ def parse_options():
      #parser.add_argument('-o', '--out_dir', dest='out_dir', action="store", metavar="DIR", required=True)
      parser.add_argument('-n', '--neptune', dest='neptune', action="store", metavar="URI:PORT", required=True)
      parser.add_argument('-v', '--vertices', dest='vertices', action="store", metavar="FILE", required=True)
+     parser.add_argument('-e', '--edges', dest='vertices', action="store", metavar="FILE", required=True)
      
      return parser.parse_args()
 
